6dfeffac4ff6eab8ba4d4e55f757b803787f5a28
vdwjeff <vdwjeff@gmail.com>
2020-05-31 12:53:01 +0200

    Updates

---
+++ precache-manifest.0d3cf97425efe52728fb3cb8c39cafb7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a937de9e98938ca27f275b2604cb0a1",
+    "url": "/bgg-collection/index.html"
+  },
+  {
+    "revision": "9ddd932a04f79ef36e6a",
+    "url": "/bgg-collection/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2e82e05d5bc3f30ca8f4",
+    "url": "/bgg-collection/static/js/2.66d984cb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/bgg-collection/static/js/2.66d984cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ddd932a04f79ef36e6a",
+    "url": "/bgg-collection/static/js/main.dad2947e.chunk.js"
+  },
+  {
+    "revision": "034d6b0f997ef466e887",
+    "url": "/bgg-collection/static/js/runtime-main.75446f5b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/bgg-collection/static/media/logo.5d5d9eef.svg"
+  }
+]);
